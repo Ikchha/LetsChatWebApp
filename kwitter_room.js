c7d501ed9836598eb0ac1befd515a22bab6cc7c0
Ikchha <102365567+Ikchha@users.noreply.github.com>
2023-12-29 11:59:23 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,14 +1,15 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDBdGnABIz7I_via8AvodSfXmYW2NS0OKM",
-  authDomain: "lets-chat-web-app-764e5.firebaseapp.com",
-  databaseURL: "https://lets-chat-web-app-764e5-default-rtdb.firebaseio.com/",
-  projectId: "lets-chat-web-app-764e5",
-  storageBucket: "lets-chat-web-app-764e5.appspot.com",
-  messagingSenderId: "570173575054",
-  appId: "1:570173575054:web:0b54a29ab0cb0a5de35edf"
-  };
-  
-   firebase.initializeApp(firebaseConfig);
+
+  apiKey: "AIzaSyAozwWFHmCO_pL2XC1aElu-TtU4T62akdY",
+  authDomain: "lets-chat-web-app-4146a.firebaseapp.com",
+  databaseURL: "https://lets-chat-web-app-4146a-default-rtdb.firebaseio.com",
+  projectId: "lets-chat-web-app-4146a",
+  storageBucket: "lets-chat-web-app-4146a.appspot.com",
+  messagingSenderId: "575999102317",
+  appId: "1:575999102317:web:fd3c70e95a39ed2fcf1e64",
+  measurementId: "G-2PC4WMXT8B"
+};
+firebase.initializeApp(firebaseConfig);
 
    user_name = localStorage.getItem("user_name");
    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
@@ -29,12 +30,9 @@ var firebaseConfig = {
     firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
     snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
     Room_names = childKey;
-//Start code
      console.log("Room Name - " + Room_names);
       row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
       document.getElementById("output").innerHTML += row;
-
-//End code
 });});}
 getData();
 
@@ -43,6 +41,13 @@ function redirectToRoomName(name) {
   localStorage.setItem("room_name" , name);
   window.location = "kwitter_page.html";
 
+}
+
+function logout()
+{
+     localStorage.removeItem("user_name");
+     localStorage.removeItem("room_name");
+     window.location = "index.html";
 }
 
 
